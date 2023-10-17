1ceacbf9a751fe68d9d30bb3e6cfa55db49a3fb2
SmokerCL <nicolasalmarza99@gmail.com>
2023-10-17 01:39:29 -0300

    Initial commit

---
+++ enviroments.ts
@@ -0,0 +1,10 @@
+// Firebase connection w/apikey
+const firebaseConfig = {
+    apiKey: "AIzaSyDTsgW59nSJNw6OzATm6-2arbXeqjgupQE",
+    authDomain: "tellevoappdb.firebaseapp.com",
+    projectId: "tellevoappdb",
+    storageBucket: "tellevoappdb.appspot.com",
+    messagingSenderId: "964920465549",
+    appId: "1:964920465549:web:2977a93f25eb6523192ac4",
+    measurementId: "G-RXX5VP6S8X"
+  };
